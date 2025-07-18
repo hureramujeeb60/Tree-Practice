c9e67183070d6ea545f939dafaf9722205a1cc1e
hureramujeeb60 <hureram8@gmail.com>
2025-07-18 18:21:47 +0500

    first commit

---
+++ postorder.py
@@ -0,0 +1,32 @@
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        lst = []
+
+        def traverse(root):
+            if root is None:
+                return
+
+            traverse(root.left)
+            traverse(root.right)
+            lst.append(root.val)
+        
+        traverse(root)
+        return lst
+    
+    
+if __name__ == "__main__":
+    node = TreeNode(1)
+    node.right = TreeNode(2)
+    node.right.left = TreeNode(3)
+    sol = Solution()
+    res = sol.postorderTraversal(node)
+    print(res)
---
+++ preorder.py
@@ -0,0 +1,32 @@
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        lst = []
+
+        def traverse(root):
+            if root is None:
+                return
+
+            lst.append(root.val)
+            traverse(root.left)
+            traverse(root.right)
+            
+        traverse(root)
+        return lst
+    
+    
+if __name__ == "__main__":
+    node = TreeNode(1)
+    node.right = TreeNode(2)
+    node.right.left = TreeNode(3)
+    sol = Solution()
+    res = sol.preorderTraversal(node)
+    print(res)
