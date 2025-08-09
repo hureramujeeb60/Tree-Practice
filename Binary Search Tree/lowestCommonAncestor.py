4b0f70adbfc8e7d7e01062619da055f69a2ba94a
hureramujeeb60 <hureram8@gmail.com>
2025-08-10 02:56:17 +0500

    add LCA problem

---
+++ Binary Search Tree/lowestCommonAncestor.py
@@ -0,0 +1,31 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+
+if __name__ == "__main__":
+    root = TreeNode(6)
+    root.left = TreeNode(2)
+    root.right = TreeNode(8)
+    root.left.left = TreeNode(0)
+    root.left.right = TreeNode(4)
+    root.left.right.left = TreeNode(3)
+    root.left.right.right = TreeNode(5)
+
+    p = root.left 
+    q = root.right       
+
+    solution = Solution()
+    lca = solution.lowestCommonAncestor(root, p, q)
+
+    print(f"LCA of {p.val} and {q.val} is {lca.val}")
